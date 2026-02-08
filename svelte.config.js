1bf3f70b09beae2190b3d079e747e13fe38cb841
ClaraUCAB <146204299+ClaraUCAB@users.noreply.github.com>
2026-02-08 10:27:30 -0400

    fix: switch to the bun adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from 'svelte-adapter-bun';
+//import adapter from '@sveltejs/adapter-static';
 import { sveltePreprocess } from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
